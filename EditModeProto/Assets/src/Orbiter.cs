c0215e4b703a911a1b49d9eb98e01a0bcf3c87e1
jarllarsson <jarl.larsson@gmail.com>
2012-11-15 13:26:26 +0100

    Orbit camera created

---
+++ EditModeProto/Assets/src/Orbiter.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class Orbiter : MonoBehaviour {
+    public Transform m_target;
+    public float m_radius;
+    public float m_speed = 0.02f;
+
+    ///
+	/// Use this for initialization
+    /// 
+	void Start () {
+
+	}
+	
+	/// 
+	/// Update is called once per frame
+	/// 
+	void Update () {
+        // Get mouse pos delta, isolate this later
+        float mx = Input.GetAxis("Mouse X") * m_speed;
+        float my = -Input.GetAxis("Mouse Y") * m_speed;
+
+        // transform.Rotate(m_rotation);
+        transform.rotation *= Quaternion.Euler(my,mx,0.0f);
+        transform.position = transform.forward*m_radius*-1;
+        
+
+	}
+}
