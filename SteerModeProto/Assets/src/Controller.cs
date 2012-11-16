44d86a54ca19a3d7a7c0e0794703cab51a434e93
jarllarsson <jarl.larsson@gmail.com>
2012-11-16 17:24:05 +0100

    steering basics

---
+++ SteerModeProto/Assets/src/Controller.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour 
+{
+    private Quaternion m_startRotation; ///< Initial rotation.
+    private Quaternion m_rotation; ///< Current rotation. 
+
+    public float m_turnspeed;
+    public float m_movespeed;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_startRotation = transform.rotation;
+        m_rotation = m_startRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 inputAngles = new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0.0f) * m_turnspeed * Time.deltaTime; // For trackball; set relative movement (in 2d space)
+
+        // Transform quaternion with movement vector
+        m_rotation *= Quaternion.Euler(inputAngles);
+
+        // Translate and rotate
+        transform.position += m_rotation * Vector3.forward * m_movespeed * Time.deltaTime;
+        transform.rotation = m_rotation;
+
+	}
+}
