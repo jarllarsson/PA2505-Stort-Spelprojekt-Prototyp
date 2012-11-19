6b96d7d703d27ce3bd59d844431538fbbb92abc5
jarllarsson <jarl.larsson@gmail.com>
2012-11-19 10:23:51 +0100

    roll added, and fov effect

--- SteerModeProto/Assets/src/Controller.cs
+++ SteerModeProto/Assets/src/Controller.cs
@@ -8,6 +8,8 @@ public class Controller : MonoBehaviour
 
     public float m_turnspeed;
     public float m_movespeed;
+    public float m_power;
+    private Vector3 m_thrustVec;
 
 	// Use this for initialization
 	void Start () 
@@ -17,16 +19,28 @@ public class Controller : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
+        float xangle = Input.GetAxis("Vertical") - Input.GetAxis("Mouse Y");
+        float yangle = Input.GetAxis("Horizontal") + Input.GetAxis("Mouse X");
+        float zangle = Input.GetAxis("Roll");
 
-        Vector3 inputAngles = new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0.0f) * m_turnspeed * Time.deltaTime; // For trackball; set relative movement (in 2d space)
+        Vector3 inputAngles = new Vector3(xangle, 
+                                          yangle, 
+                                          zangle) * m_turnspeed * Time.deltaTime; // For trackball; set relative movement (in 2d space)
 
         // Transform quaternion with movement vector
         m_rotation *= Quaternion.Euler(inputAngles);
 
-        // Translate and rotate
-        transform.position += m_rotation * Vector3.forward * m_movespeed * Time.deltaTime;
+        // Rotate
         transform.rotation = m_rotation;
 
+        // Translate
+        m_thrustVec += transform.forward * Input.GetAxis("Thrust") * Time.deltaTime * m_power; // forward is dependent on the previous rotation. alt is: m_rotation*vec3(0,0,1)
+        m_thrustVec = Vector3.Lerp(m_thrustVec, Vector3.zero, Time.deltaTime*0.3f);
+        transform.position += /*m_rotation */ m_thrustVec * Time.deltaTime;
+        
+
+        Debug.DrawLine(transform.position, transform.position + m_thrustVec*10.0f);
 	}
 }
---
+++ SteerModeProto/Assets/src/FovEffect.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class FovEffect : MonoBehaviour {
+    public Camera m_cam;
+    public float m_multiplier=1.0f;
+
+    private Vector3 m_oldPos;
+    private float m_startFov;
+	// Use this for initialization
+	void Start () 
+    {
+        m_oldPos = transform.position;
+        m_startFov = m_cam.fov;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+    {
+        m_cam.fov = Mathf.Clamp(m_startFov * (1.0f+Vector3.SqrMagnitude(transform.position - m_oldPos) * m_multiplier),1.0f,170.0f);
+        m_oldPos = transform.position;
+	}
+}
