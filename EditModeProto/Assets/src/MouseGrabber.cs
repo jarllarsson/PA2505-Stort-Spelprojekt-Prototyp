93133b687287af297796e3b1f62659a9f92e3e72
jarllarsson <jarl.larsson@gmail.com>
2012-11-22 17:13:23 +0100

    Attaching to sockets

--- EditModeProto/Assets/src/MouseGrabber.cs
+++ EditModeProto/Assets/src/MouseGrabber.cs
@@ -4,8 +4,11 @@ using System.Collections;
 public class MouseGrabber : MonoBehaviour 
 {
     public Transform m_pointOfView;
-    public int m_castToLayer=8;
+    public int m_blockLayer=8;
+    public int m_connectionMinusLayer=9;
+    public int m_connectionPlusLayer=10;
     public Transform m_currentGrabbed = null;
+    public Transform m_ship; // used to get ship orientation to correctly orient the blocks
 
     private Ray ray=new Ray();
 
@@ -28,13 +31,17 @@ public class MouseGrabber : MonoBehaviour
         // Get mouse/gamepad input
         bool grabButton = Input.GetAxis("Fire1")>0.0f;
 
+        // Default move-to/rotate-to for possible grabbed block
+        Vector3 blockMoveTo = transform.position+transform.forward*2.0f;
+        Quaternion blockRotateTo = Quaternion.LookRotation(m_pointOfView.rotation * Vector3.forward, m_ship.up);
+
         // Set up the ray
-        var layermask = 1 << m_castToLayer;
+        var layermask = 1 << m_blockLayer;
         if (m_mode == Mode.FREE)                              // ray if free
             ray.origin = transform.position;
         else if (m_mode == Mode.GRABBING && m_currentGrabbed) // ray if something is grabbed
         {
-            layermask = ~layermask;
+            layermask = 1 << m_connectionMinusLayer;
             ray.origin = m_currentGrabbed.position;
         }
 
@@ -50,6 +57,7 @@ public class MouseGrabber : MonoBehaviour
                 if (grabButton) // if button pressed, grab currently hit object
                 {
                     m_currentGrabbed = hit.transform;
+                    m_currentGrabbed.parent = null;
                     m_mode = Mode.GRABBING;
                 }
                 else // if button released, release currently grabbed object
@@ -62,9 +70,19 @@ public class MouseGrabber : MonoBehaviour
             {
                 if (!grabButton) // if button released, attach on connector
                 {
-                    m_currentGrabbed.position = hit.point;
+                    m_currentGrabbed.position = hit.transform.position;
+                    Transform other = hit.collider.transform;
+                    m_currentGrabbed.rotation = Quaternion.LookRotation(-hit.collider.transform.forward, hit.collider.transform.parent.up); // up vector of socket owner, for example: the ship
+
+                    m_currentGrabbed.parent = hit.transform;
+                    Debug.Log(m_currentGrabbed.name + " attached to " + hit.transform.name);
                     m_currentGrabbed = null;
                     m_mode = Mode.FREE;
+                }
+                else
+                {
+                    // hint what the final alignment will be if button
+                    // is released
                 }
             }
         }
@@ -77,6 +95,7 @@ public class MouseGrabber : MonoBehaviour
             {
                 if (!grabButton) // if button released, release currently grabbed object
                 {
+                    m_currentGrabbed.parent = null;
                     m_currentGrabbed = null;
                     m_mode = Mode.FREE;
                 }
@@ -86,7 +105,11 @@ public class MouseGrabber : MonoBehaviour
         // Make grabbed block follow the mouse
         if (m_mode == Mode.GRABBING && m_currentGrabbed)
         {
-            m_currentGrabbed.position = Vector3.Slerp(m_currentGrabbed.position, transform.position+transform.forward*2.0f, 20.0f*Time.deltaTime);
+            m_currentGrabbed.position = Vector3.Slerp(m_currentGrabbed.position, blockMoveTo, 20.0f * Time.deltaTime);
+
+            m_currentGrabbed.rotation = Quaternion.Slerp(m_currentGrabbed.rotation, 
+                                                         blockRotateTo, 
+                                                         20.0f * Time.deltaTime);
         }
 	}
 }
---
+++ EditModeProto/Assets/src/connect.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class connect : MonoBehaviour 
+{
+    public Transform myConnector;
+    public Transform otherConnector;
+	// Use this for initialization
+	void Start () 
+    {
+        // First fix so the connection is the pivot:
+        myConnector.parent = null;                  // set connector to world space temporarily
+        myConnector.position -= transform.position; // reposition connection to world origo + offset from parent
+        transform.position = Vector3.zero;          // set this to origo
+	    transform.parent = myConnector;             // set this to be child to connector
+
+        // Reposition on other connection
+        myConnector.position = otherConnector.position;
+
+        // Now rotate:
+        myConnector.localRotation = Quaternion.LookRotation(-otherConnector.forward);
+        myConnector.RotateAround(myConnector.forward, Random.Range(0.0f, 360.0f)); // optional, rotate random along forward
+
+        // reset parent
+        transform.parent = null;
+        myConnector.parent = transform;
+        transform.parent = otherConnector;
+        // transform.parent = otherConnector.parent; // alternative
+
+        // Done! :)
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+}
